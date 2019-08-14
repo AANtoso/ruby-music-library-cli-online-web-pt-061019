4b652f73d17c8c4c5eef06e49251fde6d1efc695
Adjoa Ntoso <adjoaan@gmail.com>
2019-08-14 01:04:47 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,17 @@ Bundler.require
 module Concerns
   module Findable
      def self.find_by_name(name)
-    self.all.find {|song| song.name == name} 
+    self.all.find {|song| song.name == name}
   end
-  
+
    def self.find_or_create_by_name(name)
      if !self.find_by_name(name)
         Song.create(name)
-      else 
+      else
         self.find_by_name(name)
-    end 
-  end 
-  end 
+    end
+  end
+  end
 end
 
 require_all 'lib' 
